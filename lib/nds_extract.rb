7462be86972200930e0f6b2d2933e9a0432c063c
Colton stephens <stephens.colton@icloud.com>
2020-02-27 22:56:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,7 +33,6 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-@@ -38,6 +46,14 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
